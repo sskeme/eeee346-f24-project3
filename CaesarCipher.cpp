cb4862c1b3b0f74bc3d378287b30f0ba2fb3ceb5
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-11-20 23:36:02 -0500

    Create CaesarCipher.cpp

---
+++ CaesarCipher.cpp
@@ -0,0 +1,20 @@
+#include "CaesarCipher.h"
+
+/*
+ * definition of the constructor
+ */
+/*TODO: write your code*/
+
+/*
+ * definition of encode()
+ */
+bool CaesarCipher::encode(string msg) {
+  /*TODO: write your code*/
+}
+
+/*
+ * definition of decode()
+ */
+bool CaesarCipher::decode(string msg) {
+  /*TODO: write your code*/
+}
