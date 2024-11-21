51411767af42e49e44fcaf1ab9e8f2d1136a8561
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-11-20 23:33:00 -0500

    Create CaesarCipher.h

---
+++ CaesarCipher.h
@@ -0,0 +1,37 @@
+#ifndef CAESARCYPHER_H
+#define CAESARCYPHER_H
+
+#include "Cipher.h"
+
+/*
+ * derived concrete class of the abstract base class Cipher
+ */
+class CaesarCipher : public Cipher {
+private:
+  /*  
+   * shift value for a Caesar cipher
+   */
+  int shift;
+
+public:
+  /*  
+   * constructor
+   * @param s - used to set shift
+   */
+  CaesarCipher(int s); 
+
+  /** 
+   * encode(): performs Caesar encoding of a message
+   * @param msg - string message to encode
+   * @return true if encoding is successful else false
+   */
+  bool encode(string msg);
+
+  /** 
+   * decode(): performs Caesar decoding of message
+   * @param msg - string message to decode
+   * @return true if decoding is successful else false
+   */
+  bool decode(string msg);
+};
+#endif
