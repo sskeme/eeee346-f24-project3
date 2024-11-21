cc9e1e39d2dafbd2b285617422ae5265c11efe2a
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-11-20 23:47:42 -0500

    Create VigenereCipher.h

---
+++ VigenereCipher.h
@@ -0,0 +1,46 @@
+#ifndef VIGENERECYPHER_H
+#define VIGENERECYPHER_H
+
+#include "Cipher.h"
+
+class VigenereCipher : public Cipher {
+private:
+  /*  
+   * original key used in a Vigenere cipher
+   */
+  string key;
+  
+  /*  
+   * converted key from the original key
+   */
+  string convKey;
+
+public:
+  /* 
+   * constructor
+   * @param k - used to set key
+   */
+  VigenereCipher(string k); 
+
+  /*  
+   * convertKey: converts key to match the lenghth of msg
+   * and the length of key
+   * the result is stored in convKey
+   */
+  void convertKey(const string& msg, const string& key);
+  
+  /*  
+   * encode(): performs Vigenere encoding of a message
+   * @param msg - string message to encode
+   * @return true if encoding is successful else false
+   */
+  bool encode(string msg);
+
+  /*  
+   * decode(): performs Vigenere decoding of message
+   * @param msg - string message to decode
+   * @return true if decoding is successful else false
+   */
+  bool decode(string msg);
+};
+#endif
