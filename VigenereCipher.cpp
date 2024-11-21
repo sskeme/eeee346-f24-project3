51405bc59c87086ca69b2cbd6b4dfe793ab08560
Sunny Kim <154963758+sskeme@users.noreply.github.com>
2024-11-20 23:50:35 -0500

    Create VigenereCipher.cpp

---
+++ VigenereCipher.cpp
@@ -0,0 +1,27 @@
+#include "VigenereCipher.h"
+
+/*
+ * definition of the constructor
+ */
+/*TODO: write your code*/
+  
+/*
+ * definition of convertKey()
+ */
+void VigenereCipher::convertKey(const string& msg, const string& key) {
+  /*TODO: write your code*/
+}
+
+/*
+ * definition of encode()
+ */
+bool VigenereCipher::encode(string msg) {
+  /*TODO: write your code*/
+} 
+
+/*
+ * definition of decode()
+ */
+bool VigenereCipher::decode(string msg) {
+  /*TODO: write your code*/
+}
